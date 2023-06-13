ee84b154937cce639e01c14fdb7155b5d95d3179
Aurelio Siordia <lz_rock@hotmail.com>
2023-06-13 11:21:49 -0600

    the algorithmic approach

--- galileo_equations.py
+++ galileo_equations.py
@@ -12,12 +12,18 @@ def plot_trajectory():
     """Plot trjectory of the ball"""
     xs = [x/100 for x in list(range(201))]
     ys = [ball_trajectory(x) for x in xs]
-    plt.plot(xs, ys)
+    xs2 = [0.1, 2]
+    ys2 = [ball_trajectory(0.1), 0]
+    xs3 = [0.2, 2]
+    ys3 = [ball_trajectory(0.2), 0]
+    xs4 = [0.3, 2]
+    ys4 = [ball_trajectory(0.3), 0]
+
+    plt.plot(xs, ys, xs2, ys2, xs3, ys3, xs4, ys4)
     plt.title("The trajectory of a Thrown Ball")
     plt.xlabel("Horizontal Position of Ball")
     plt.ylabel("Vertical Position of Ball")
     plt.axhline(y=0)
-    print(xs[0])
     plt.show()
     
 
