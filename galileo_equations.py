18a7af327b58aed4cf253a5b34c051d8d65749f1
Aurelio Siordia <lz_rock@hotmail.com>
2023-06-13 16:45:59 -0600

    angles and tangents

--- galileo_equations.py
+++ galileo_equations.py
@@ -18,8 +18,14 @@ def plot_trajectory():
     ys3 = [ball_trajectory(0.2), 0]
     xs4 = [0.3, 2]
     ys4 = [ball_trajectory(0.3), 0]
+    xs5 = [0.3,0.3]
+    ys5 = [0, ball_trajectory(0.3)]
+    xs6 = [0.3, 2]
+    ys6 = [0, 0]
 
-    plt.plot(xs, ys, xs2, ys2, xs3, ys3, xs4, ys4)
+    plt.text(0.31, ball_trajectory(0.3)/2, 'A', fontsize=16)
+    plt.text((0.3+2)/2, 0.05, 'B', fontsize=16)
+    plt.plot(xs, ys, xs2, ys2, xs3, ys3, xs4, ys4, xs5, ys5, xs6, ys6)
     plt.title("The trajectory of a Thrown Ball")
     plt.xlabel("Horizontal Position of Ball")
     plt.ylabel("Vertical Position of Ball")
