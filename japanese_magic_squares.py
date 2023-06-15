4a569e9360d2543e6d50509b54befcb4b8960c15
Aurelio Siordia <lz_rock@hotmail.com>
2023-06-15 08:08:40 -0600

    fill central squares

--- japanese_magic_squares.py
+++ japanese_magic_squares.py
@@ -1,5 +1,31 @@
+import math
 
-luoshu = [[4,9,2],[3,5,7],[8,1,6]]
+#luoshu = [[4,9,2],[3,5,7],[8,1,6]]
+
+n = 7
+square = [[float('NaN') for i in range(0,n)] for j in range(0,n)]
+
+def printsquare(square):
+    """Print formated square"""
+    # Print labels of columns
+    labels = ['['+str(x)+']'  for x in range(0, len(square))]
+    format_row = "{:>6}" * (len(labels) + 1)
+    print (format_row.format("", *labels))
+
+    #Print labels of rows and values of square
+    for label, row in zip(labels, square):
+        print (format_row.format(label, *row))
+
+def fill_central_squares(square):
+    """Fill central cells of magic square"""
+    center_i = math.floor(n/2)
+    center_j = math.floor(n/2)
+
+    square[center_i][center_j] = int((n**2+1)/2)
+    square[center_i + 1][center_j] = 1
+    square[center_i -1][center_j] = n**2
+    square[center_i][center_j +1] = n**2 + 1 - n
+    square[center_i][center_j -1] = n
 
 def verifysquare(square):
     """Verify whether a given matrix is a magic square"""
@@ -28,4 +54,5 @@ def verifysquare(square):
     return(len(list(set(flattened))) == 1)
 
 if __name__ == '__main__':
-    print(verifysquare(luoshu))
+    fill_central_squares(square)
+    printsquare(square)
