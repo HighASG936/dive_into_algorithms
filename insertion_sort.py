9ad994949d7623f2dee677d1d296cef8fcc6da54
Aurelio Siordia <lz_rock@hotmail.com>
2023-06-22 12:37:47 -0600

    Comparing Insertion Sort to Other Growth Rates

--- insertion_sort.py
+++ insertion_sort.py
@@ -45,21 +45,30 @@ def check_steps(size_of_cabinet):
 
 
 if __name__ == '__main__':
-	stepcounter = 0
-	start = timer()
+    stepcounter = 0
+    start = timer()
 
-	random.seed(5040)
-	xs = list(range(1,100))
-	ys = [check_steps(x) for x in xs]
-	ys = [math.exp(x) for x in xs]
+    random.seed(5040)
+    xs = list(range(1,100))
+    ys = [check_steps(x) for x in xs]
+    ys_exp = [math.exp(x) for x in xs]
+    ys_squared = [x**2 for x in xs]
+    ys_threehalves = [x**1.5 for x in xs]
+    ys_cubed = [x**3 for x in xs]
+    ys_more_acurrent = [ 18.7*((0.5*x**2)+(1.5*x))   for x in xs]
 
-	plt.plot(xs, ys)
+    plt.plot(xs, ys)
     axes = plt.gca()
     axes.set_ylim(np.min(ys), np.max(ys) + 140)
-	plt.title('Comparing Insertion Sort to th Exponential Function')
-	plt.xlabel('Number of Files in Random Cabinet')
-	plt.ylabel('Steps Required to Sort Cabinet by Insertion Sort')
-	plt.show()
+    plt.plot(xs, ys_exp)
+    plt.plot(xs, ys_squared)
+    plt.plot(xs, ys_threehalves)
+    plt.plot(xs, ys_cubed)
+    plt.plot(xs, ys_more_acurrent)
+    plt.title('Comparing Insertion Sort to Other Growth Rates')
+    plt.xlabel('Number of Files in Random Cabinet')
+    plt.ylabel('Steps Required to Sort Cabinet by Insertion Sort')
+    plt.show()
 
 #	cabinet = [8,4,6,1,2,5,3,7]
 #	sortedcabinet = insertion_sort(cabinet)
