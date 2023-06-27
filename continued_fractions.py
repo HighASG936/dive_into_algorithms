6ae60024c7d40283406340ee082c5c1040c5e36a
Aurelio Siordia <lz_rock@hotmail.com>
2023-06-26 20:11:19 -0600

    "continued_fractions_with_decimals"

--- continued_fractions.py
+++ continued_fractions.py
@@ -15,7 +15,7 @@ def continued_fraction(x, y, length_tolerance):
 	return output
 
 def get_number(continued_fraction):
-	""" """
+	"""Calculate the result of the continued fraction"""
 	index = -1
 	number = continued_fraction[index]
 
@@ -25,8 +25,24 @@ def get_number(continued_fraction):
 		index -= 1
 	return number
 
+def continued_fraction_decimal(x, error_tolerance, length_tolerance):
+	"""Algorithm for generation continued fractions from a decimal number"""
+	output = []
+	first_term = int(x)
+	leftover = x - int(x)
+	output.append(first_term)
+	error = leftover
+
+	while error > error_tolerance and len(output) < length_tolerance:
+		next_term = math.floor(1/leftover)
+		leftover = 1/leftover - next_term
+		output.append(next_term)
+		error = abs(get_number(output) - x)
+	return output
 
 if __name__ == "__main__":	
-	array = continued_fraction(229, 47, 10)
+	#array = continued_fraction(229, 47, 10)
+	array = continued_fraction_decimal(1.4142135623730951, 0.00001, 100)
+
 	number = get_number(array)
 	print(f"{array}\n{number}")
