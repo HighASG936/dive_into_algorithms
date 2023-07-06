2e5c7e3d2801af4e3dcee7669a4e8866f42fca80
Aurelio Siordia <lz_rock@hotmail.com>
2023-07-05 18:29:12 -0600

    Plot a circumscribed triangles

--- postmaster_problem.py
+++ postmaster_problem.py
@@ -3,10 +3,10 @@ from matplotlib import collections as mc
 
 pythagorean = lambda a, b: math.sqrt(a**2 + b**2)
 
-def points_to_triangule(point1, point2, point3):
+def points_to_triangle(point1, point2, point3):
     """Set points coordinates and put on a list"""
-    triangule = [list(point1), list(point2), list(point3)]
-    return triangule
+    triangle = [list(point1), list(point2), list(point3)]
+    return triangle
 
 def genlines(cities, itinerary):
     """Get lines between two consecutively points"""
@@ -22,14 +22,14 @@ def get_distance(point1, point2):
     distance = pythagorean(a, b)
     return distance
 
-def plot_triangule_simple(triangule, thename):
-    """Plot a simple triangule and its vertices"""
+def plot_triangle_simple(triangle, thename):
+    """Plot a simple triangle and its vertices"""
     fig, ax = pl.subplots()
-    xs = [triangule[0][0], triangule[1][0], triangule[2][0]]
-    ys = [triangule[0][1], triangule[1][1], triangule[2][1]]
+    xs = [triangle[0][0], triangle[1][0], triangle[2][0]]
+    ys = [triangle[0][1], triangle[1][1], triangle[2][1]]
     itin = [0,1, 2, 0]
-    thelines = genlines(triangule, itin)
-    lc = mc.LineCollection(genlines(triangule, itin), linewidths=2)
+    thelines = genlines(triangle, itin)
+    lc = mc.LineCollection(genlines(triangle, itin), linewidths=2)
     
     ax.add_collection(lc)
     
@@ -38,31 +38,68 @@ def plot_triangule_simple(triangule, thename):
     pl.savefig(str(thename)+'.png')
     pl.close()
 
-def triangule_to_circumcenter(triangule):
+def triangle_to_circumcenter(triangle):
     """Find the circumcenter and circumradius"""
-    x, y, z = complex(triangule[0][0], triangule[0][1]), \
-              complex(triangule[1][0], triangule[1][1]), \
-              complex(triangule[2][0], triangule[2][1])      
+    x, y, z = complex(triangle[0][0], triangle[0][1]), \
+              complex(triangle[1][0], triangle[1][1]), \
+              complex(triangle[2][0], triangle[2][1])      
     #w = z - x
     #w /=y - x
     w = (z - x)/(y - x)    
     
     c =   (x-y) * (w-abs(w)**2)  /2j/w.imag - x
     radius = abs(c+x)
-    return -c.real, -c.imag, radius
+    return (-c.real, -c.imag), radius
+
+def plot_triangle(triangles, centers, radii, thename):
+    """ """
+    fig, ax = pl.subplots()
+    ax.set_xlim([0, 1])
+    ax.set_ylim([0, 1])
     
+    for i in range(0, len(triangles)):
+        triangle = triangles[i]
+        center = centers[i]
+        radius = radii[i]
+        itin = [0,1, 2, 0]
+        thelines = genlines(triangle, itin)
+        xs = [triangle[0][0], triangle[1][0], triangle[2][0]]
+        ys = [triangle[0][1], triangle[1][1], triangle[2][1]]
+        lc = mc.LineCollection(genlines(triangle, itin), linewidths=2)
+        ax.add_collection(lc)
+        ax.margins(0.1)
+        pl.scatter(xs, ys)
+        pl.scatter(center[0], center[1])
+        circle = pl.Circle(center, radius, color='b', fill=False)
+        ax.add_artist(circle)
+    pl.savefig(str(thename)+'.png')
+    pl.close()    
     
 if __name__ == '__main__':
+    
+    #Plot a simple triangle
     a = (0.2, 0.8)
     b = (0.5, 0.2)
     c = (0.8, 0.7)
-    x_center, y_center, radius = triangule_to_circumcenter(points_to_triangule(a, b, c))
-    print(f"center: x={x_center:.2f}, y={y_center:.2f}")
+    center, radius = triangle_to_circumcenter(points_to_triangle(a, b, c))
+    print(f"center: {center}")
     print(f"radius: {radius:.2f}")
-    plot_triangule_simple(points_to_triangule(a, b, c), 'tri')
+    plot_triangle_simple(points_to_triangle(a, b, c), 'tri')
 
-
+    #Plot a circumscribed triangles
+    a = (0.1, 0.1)
+    b = (0.3, 0.6)
+    c = (0.5, 0.2)
+    triangle1 = points_to_triangle(a, b, c)
+    center1, radius1 = triangle_to_circumcenter(triangle1) 
     
+    a = (0.8, 0.1)
+    b = (0.7, 0.5)
+    c = (0.8, 0.9)
+    triangle2 = points_to_triangle(a, b, c)
+    center2, radius2 = triangle_to_circumcenter(triangle2)
+    
+    plot_triangle([triangle1, triangle2], [center1, center2], [radius1, radius2],'two')
     
     
     
