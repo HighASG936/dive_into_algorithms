90263de8b227280d47200fb65b06d073f78ac8c6
Aurelio Siordia <lz_rock@hotmail.com>
2023-08-04 16:41:03 -0600

    1st. refactoring from pipopipette

--- pipopipette.py
+++ pipopipette.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from matplotlib import collections as mc
+from functools import reduce
 
 def drawlattice(name):
     """
@@ -12,12 +13,10 @@ def drawlattice(name):
             plt.plot(i, j, 'o', c='black')
     plt.savefig(name)
 
-def drawgame(name, game):
+def define_player_traces_colors(game):
     """
     
     """
-    global n
-
     colors2 = []
 
     for k in range(0, len(game)):
@@ -25,47 +24,88 @@ def drawgame(name, game):
             colors2.append('red')
         else:
             colors2.append('blue')
-    
-    lc = mc.LineCollection(game, colors=colors2, linewidths=2)
-    fig, ax = plt.subplots()
+    return colors2
 
+def draw_grid():
+    """
+
+    """
     for i in range(1, n + 1):
         for j in range(1, n + 1):
             plt.plot(i, j, 'o', c='black')
+
+def drawgame(name, game):
+    """
     
+    """
+    global n
+
+    colors2 = define_player_traces_colors(game)
+    lc = mc.LineCollection(game, colors=colors2, linewidths=2)
+    fig, ax = plt.subplots()
+    draw_grid()
     ax.add_collection(lc)
     ax.autoscale()
     ax.margins(0.1)
     plt.savefig(name)
 
+def is_parallel(game):
+    """
+    
+    """
+    global start_line1, end_line1, start_line2, end_line2
+    return ([(start_line1, end_line1 -1), (start_line2, end_line2 -1)] in game)
+
+def is_left(game):
+    """
+    
+    """
+    global start_line1, end_line1, start_line2, end_line2
+    return ([(start_line1, end_line1), (start_line2 -1, end_line2 -1)] in game)
+
+def is_right(game):
+    """
+    
+    """
+    global start_line1, end_line1, start_line2, end_line2
+    return ([(start_line1 +1, end_line1), (start_line2, end_line2 -1)] in game)            
+
+def logical_and(x, y):
+    return x and y
+
+def is_a_square(game):
+    global start_line1 
+    global end_line1    
+    global start_line2
+    global end_line2    
+    
+    parallel = is_parallel(game)
+    left = is_left(game)
+    right = is_right(game)
+
+    results = [parallel, left, right]
+    return reduce(logical_and, results)
+
 def squarefinder(game):
     """
     
     """
+    global start_line1, end_line1, start_line2, end_line2
+
     countofsquares = 0
 
-    for line in game:
-        parallel = False
-        left = False
-        right = False        
-        if line[0][1] == line[1][1]:                        
-            if [
-                (line[0][0], line[0][1] -1), 
-                (line[1][0], line[1][1] -1)
-               ] in game:
-                parallel = True        
-            if [
-                (line[0][0], line[0][1]), 
-                (line[1][0] -1, line[1][1] -1)
-               ] in game:
-                left = True
-            if [
-                (line[0][0] +1, line[0][1]), 
-                (line[1][0], line[1][1] -1)
-               ] in game:
-                right = True
-            if parallel and left and right:
+    for line in game:  
+        
+        start_line1 = line[0][0]
+        end_line1 = line[0][1]
+        
+        start_line2 = line[1][0]
+        end_line2 = line[1][1]
+
+        if end_line1 == end_line2:
+            if is_a_square(game): 
                 countofsquares += 1
+
         return countofsquares
 
 def score(game):
